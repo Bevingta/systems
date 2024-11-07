fab112114754aca02e810aa5b99aea51e6bb6bc7
Gleidson De Sousa <desousag@bc.edu>
2024-11-06 21:54:02 -0500

    commit

--- pa5/pa5.c
+++ pa5/pa5.c
@@ -7,21 +7,32 @@
 void transpose(const int dim, int * const m) {
     for (int i = 0; i < dim; ++i) {
         for (int j = 0; j < i; ++j) {
-	    int temp = m[i * dim + j];
-	    m[i * dim + j] = m[j * dim + i];
-	    m[j * dim + i] = temp;
-	}
+	        int temp = m[i * dim + j];
+	        m[i * dim + j] = m[j * dim + i];
+	        m[j * dim + i] = temp;
+	    }  
     }
 }
 
 void multiply(const int dim, const int * const a, const int * const b, int * const c) {
     for (int i = 0; i < dim; ++i) {
         for (int j = 0; j < dim; ++j) {
-	    c[i * dim + j] = 0;
-	    for (int k = 0; k < dim; ++k) {
-	        c[i * dim + j] += a[i * dim + k] * b[k * dim + j];
+	        c[i * dim + j] = 0;
+	        for (int k = 0; k < dim; ++k) {
+	            c[i * dim + j] += a[i * dim + k] * b[k * dim + j];
+	        }
 	    }
-	}
+    }
+}
+
+void multiply_transpose(const int dim, const int * const a, const int * const b_t, int * const c) {
+    for (int i = 0; i < dim; ++i) {
+        for (int j = 0; j < dim; ++j) {
+                c[i * dim + j] = 0;
+            for (int k = 0; k < dim; ++k) {
+                c[i * dim + j] += a[i * dim + k] * b_t[j * dim + k];
+            }
+        }
     }
 }
 
@@ -38,11 +49,12 @@ void print(const int dim, const int * const m) {
 int main(int argc, char ** argv) {
     int *a = (int *)calloc(DIM * DIM, sizeof(int));
     int *b = (int *)calloc(DIM * DIM, sizeof(int));
-    int *c = (int *)calloc(DIM * DIM, sizeof(int));
-    
+    int *c1 = (int *)calloc(DIM * DIM, sizeof(int));
+    int *c2 = (int *)calloc(DIM * DIM, sizeof(int));
+
     for (int i = 0; i < DIM * DIM; ++i) {
         a[i] = rand() % MAX_VALUE;
-	b[i] = rand() % MAX_VALUE;
+	    b[i] = rand() % MAX_VALUE;
     }
     
     printf("A: \n");
@@ -51,14 +63,25 @@ int main(int argc, char ** argv) {
     printf("B: \n");
     print(DIM, b);
     
-    multiply(DIM, a, b, c);
+    transpose(DIM, b);
 
-    printf("MATRIX C:\n");
-    print(DIM, c);
+    printf("B_T:\n");
+    print(DIM, b);
+
+    multiply(DIM, a, b, c1);
+    
+
+    printf("C:\n");
+    print(DIM, c1);
+
+    multiply_transpose(DIM, a, b, c2);
+    printf("C Transposed:\n");
+    print(DIM, c2);
 
     free(a);
     free(b);
-    free(c);
+    free(c1);
+    free(c2);
 
     return 0;
 
