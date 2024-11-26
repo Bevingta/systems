8bc95278165cd59a32aa5b9e777be9f4117f3acb
Bevingta <123000377+Bevingta@users.noreply.github.com>
2024-11-25 20:37:04 -0500

    done

--- pa7/mem_alloc.h
+++ pa7/mem_alloc.h
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/mman.h>
 
-#define PAGS_SIZE 4096
+#define PAGE_SIZE 4096
 #define WORD_SIZE 8
 #define SUCCESS 0
 #define FAILURE -1
@@ -11,3 +12,12 @@ typedef struct Header{
   struct Header *next;
   struct Header *previous;
 } Header;
+
+extern Header * free_list;
+
+void* mem_alloc(size_t size);
+void mem_free(void* ptr);
+
+Header* get_header(void* ptr);
+void print_header(Header* header);
+void print_list();
